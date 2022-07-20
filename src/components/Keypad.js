7b0d2a4e7b8d5d14d6320bd961178f2d6343c824
Alice Mbeke <alicembeke575@gmail.com>
2022-07-20 10:51:36 +0300

    commit tree 7659cdcdb179a246b140a4968f45379765597ab0

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,10 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function inputPassword() {
+    console.log("Entering password...");
+  }
+  return <input type="password" onChange={inputPassword} />;
 }
 
 export default Keypad;
